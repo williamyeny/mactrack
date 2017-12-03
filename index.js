06a483d18d73e4009548005f045173d2958a1ac8
williamyeny <williamyeny@gmail.com>
2017-12-02 20:21:18 -0500

    added express server

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-var arp = require('node-arp');
+var arp = require("node-arp");
 
-console.log("test");
+var express = require("express");
+var app = express();
+var port = 4567;
 
-for (i = 1; i < 256; i++) {
-  arp.getMAC('10.191.80.' + i, function(err, mac) {
-     if (!err) {
-         console.log(mac);
-     }
-  });
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.get("/", function(req, res) {
+  res.render("index.ejs");
+});
+
+app.listen(port, function() { 
+  console.log("server started on port " + port);
+});
+
+function scanArp() {
+  for (i = 1; i < 256; i++) {
+    // grab mac addresses from 1 - 256
+    arp.getMAC('10.196.11.' + i, function(err, mac) { // normally 192.168.1. for home networks
+      if (!err) {
+          console.log(mac);
+      } else {
+        //  console.log("error: " + mac);
+      }
+    });
+  }
 }
